a6a34a96af3df18842e76bb7c1e2877e42604a07
blu <87286496+Bluelightning26@users.noreply.github.com>
2025-07-30 20:55:44 -0400

    now ready

--- index.js
+++ index.js
@@ -83,4 +83,4 @@ app.delete('/conversation/:id', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-});});
+});
