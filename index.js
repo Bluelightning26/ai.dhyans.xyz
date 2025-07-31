6a7fb37de4d8afb2b9ad677ccce51dd70aa4af29
blu <87286496+Bluelightning26@users.noreply.github.com>
2025-07-31 08:15:26 -0400

    remove vercel analytics

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-import { inject } from "@vercel/analytics"
+
 
 const app = express();
 app.use(express.json());
